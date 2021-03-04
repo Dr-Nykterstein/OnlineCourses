1684232a9043d33cd6186997b2c6db3e2b82dd22
roman <romankhudobliak@gmail.com>
2021-03-04 15:24:50 +0200

    roll back

--- src/components/hotelsInCity/HotelInCityItem.jsx
+++ src/components/hotelsInCity/HotelInCityItem.jsx
@@ -1,50 +1,21 @@
 import React, { useEffect, useState} from 'react'
 
-import GetHotelImg from './GetHotelImages'
-
-//import GetDetails from '../../services/GetDetails'
-
-const HotelInCityItem = (...props) => {
-    const [appState, setAppState] = useState()
-    // useEffect(()=>
-    // GetDetails({id: props.id, adult1: 1})
-    // .then(res => {
-    //     console.log(res);
-    //     setAppState(res.data.data.body.overview.overviewSections);
-    // })
-    // .catch(error => {
-    //     console.log(error)  
-    // }),[setAppState]
-    // )
-            //     {appState.map((item,key) => {
-                              
-        //         // <h3>{item.title}</h3>
-        //     })
-        //     }
+const HotelInCityItem = (props) => {
+    const [appState, setAppState] = useState([{}])
         
-    //const srcOnPict = GetHotelImg({id: props.id}); //here is pict of your hotel 
-    useEffect(()=>{
-        const hotelsList = {...props};
-        setAppState(hotelsList);
-    }
-    ,[setAppState])
-
     return (
-        <div className="hotels-wrapper">
-            <ul className="hotels-items-container">
-                <li className='hotels-item-container'>
-                    <figure className='hotels-item-pic-wrap'>
-            
-                    </figure>
-                    <div className='hotel-info'>
-                        <h1>kjjjjjj</h1>           
-                    </div>
-    
-                </li>
- 
-            </ul>
-        </div>
-    )    
+        <>
+            <li className='hotels-item-container'>
+                <figure className='hotels-item-pic-wrap'>
+                    
+                </figure>
+                <div className='hotel-info'>
+                    <h1>{props.name}</h1>
+                </div>
+        
+            </li>
+        </>
+    )
 }
 
 export default HotelInCityItem
--- src/components/hotelsInCity/HotelsInCity.jsx
+++ src/components/hotelsInCity/HotelsInCity.jsx
@@ -1,36 +1,41 @@
 import React,{ useEffect, useState }  from 'react'
 
-import GetListOfHotelInSomeCity from './GetListOfHotelsInCityCountryEct'
+import SearchByKeyWord from '../../services/SearchByKeyWord'
 
-
-// import HotelInCityItem from './HotelInCityItem'
-// import OnLoadingHotelInCityData from './HotelItemLoader' 
-
-import LoadingHotelData from './HotelItemLoader' 
+import HotelInCityItem from './HotelInCityItem'
 
 import './HotelsInCity.css'
 
 function HotelsInCityExp(props) {
-  //const DataLoading = OnLoadingHotelInCityData(HotelInCityItem);
-  const [appState, setAppState] = useState({ loading: true, hotels: []});
-  const hotels = [] 
-  useEffect(()=>{
-    const runEffect = async () => {
-      const hotelsInfo = await GetListOfHotelInSomeCity({query: props.name});
-      setAppState({ loading: false, hotels: hotelsInfo});
-    }
-    runEffect();
-  },[])
-  //<HotelInCityItem key={key} caption={item.caption} id={item.destinationId} name={item.name} geoId={item.geoId}  latitude={item.latitude} longitude={item.longitude} />
-  console.log(appState.hotels);
+
+  const [appState, setAppState] = useState([{}])
+  
+  
+  useEffect(()=>
+  SearchByKeyWord({query: props.name})
+  .then(res => {
+    setAppState(res.data.suggestions[1].entities);
+  })
+  .catch(error => {
+    console.log(error)  
+  }),[setAppState]
+  )
+  
+  console.log(appState);
   return (
     <div className="hotels">
-    <h1>Hotels</h1>
+      <h1>Hotels</h1>
       <div className="hotels-conteiner">
-        <LoadingHotelData loading = {hotels.loading} hotels = {appState.hotels} />
+        <div className="hotels-wrapper">
+          <ul className="hotels-items-container">
+            {appState.map((item,key) => (
+              <HotelInCityItem key={key} caption={item.caption} id={item.destinationId} name={item.name} geoId={item.geoId}  latitude={item.latitude} longitude={item.longitude} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
-    );
-  }
-  
-  export default HotelsInCityExp;
+  );
+}
+
+export default HotelsInCityExp;
